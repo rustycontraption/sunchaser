fc7eeda88ae8daac193e0e5d4dac2525f5bca35c
julian <rustycontraption@gmail.com>
2020-11-10 20:40:33 -0800

    Cache noaa results

--- sunChaser.py
+++ sunChaser.py
@@ -7,6 +7,7 @@ import pyproj
 import geopy
 import time
 import dateutil
+import os
 from dateutil import parser
 from datetime import datetime, timezone, timedelta
 from geopy.distance import geodesic
@@ -28,26 +29,33 @@ def query_overpass(origin,distance):
         )
     nodes = result.json()
 
-    locations = {}
+    data = {
+        "timestamp": datetime.now(),
+        "locations": {}
+    }
     for item in nodes["elements"]:
-        locations[item["tags"]["name"]] = {
+        data["locations"][item["tags"]["name"]] = {
             "lat": item["lat"],
             "lon": item["lon"]
             }
-    return locations
+    return data
     
-def query_noaa(locations):
+def query_noaa(data):
     print("querying noaa...")
     # Forecasts are divided into 2.5km grids. 
     # Each NWS office is responsible for a section of the grid.
     # get grid for each city in location, retrieve weather report for that grid,
     # then update location dictionary with relevant weather data
 
-    for loc in locations:
+    # Add timestamp for checking age of cached results
+    data["timestamp"] = str(datetime.now())
+
+    for loc in list(data["locations"]):
+        print(loc)
         # Sleep between API requests to avoid hitting Google's request rate limit
         gridData = requests.get("https://api.weather.gov/points/" + 
-            str(locations[loc]["lat"]) + "," + 
-            str(locations[loc]["lon"])).json()
+            str(data["locations"][loc]["lat"]) + "," + 
+            str(data["locations"][loc]["lon"])).json()
         time.sleep(.02)
         weatherData = requests.get(gridData["properties"]["forecastGridData"]).json()
         time.sleep(.02)
@@ -55,24 +63,42 @@ def query_noaa(locations):
         # If a location doesn't have the required data, delete that location from the
         # dict.
         if "properties" in weatherData and "skyCover" in weatherData["properties"]:
-            locations[loc]["skyCover"] = weatherData["properties"]["skyCover"]
+            data["locations"][loc]["skyCover"] = weatherData["properties"]["skyCover"]
         else:
-            del locations[loc]
+            del data["locations"][loc]
     
     print("done getting locations")
-    return locations
+
+    # Cache results
+    with open('locations.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    return data
 
 def main(origin, distance):
     # Retrieve cities
     locations = query_overpass(origin, distance)
-    # Retrieve weather forecast for cities
-    locData = query_noaa(locations)
 
-    # NOAA returns forecasts for 
+    # Retrieve weather forecast for cities.
+    # First check if there is cached data we can use instead to
+    # save ourselves the API requests.
+    if os.path.exists("locations.json"):
+        with open('locations.json') as data:
+            locCache = json.load(data)
+        if datetime.strptime(locCache['timestamp'], '%Y-%m-%d %H:%M:%S.%f') < datetime.now() + timedelta(hours = 1):
+            locData = locCache
+        else: locData = query_noaa(locations)
+    else:
+        locData = query_noaa(locations)
+
+    # Obtaining forecast for desired the time is a pain in the ass.
+
+    # Round our current time to nearest hour to compare with NOAAs
+    # hourly forecasts
     currentTime = datetime.now(timezone.utc)
     roundedTime = currentTime.replace(second=0, microsecond=0, minute=0, hour=currentTime.hour) + timedelta(hours=currentTime.minute//30)
-    for loc in locData:
-        for value in locData[loc]["skyCover"]["values"]:
+    for loc in locData["locations"]:
+        for value in locData["locations"][loc]["skyCover"]["values"]:
             validTime = value["validTime"].split("/")[0]
             validTimeFormatted = dateutil.parser.parse(validTime)
             if validTimeFormatted == roundedTime:
@@ -92,7 +118,7 @@ if __name__ == "__main__":
         "--distance",
         help="Maximum distance in meters to search for sun.",
         action="store",
-        default="100000")
+        default="10000")
 
     args = parser.parse_args()
 
